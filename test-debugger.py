a9d4446bae94b9055945e1c56ba1a5cf7948229a
LEONID HAILOV <leonid.hailov@mercedes-benz.com>
2026-08-15 22:33:51 +0200

    Create test-debugger.py
    adding the test for a debugger to troubleshoot the issue with the debugger

---
+++ test-debugger.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import configparser
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+config_file = PROJECT_ROOT / "environment.cfg"
+
+print("Config:", config_file)
+
+config = configparser.ConfigParser()
+config.read(config_file)
+
+print("Sections:", config.sections())
